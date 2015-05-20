d7df3174c1addfce1cce3068cda5967d1ad59b52
Ludovico Biondani <ludovico.biondani@alice.it>
2015-05-20 15:25:47 +0200

    add api controller

---
+++ Controllers/GestoreController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PointrestServerSide.Controllers
+{
+    public class GestoreController : ApiController
+    {
+        //// GET: api/Gestore
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Gestore/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            //return : [class Gestore,[class PuntoInteresse ]]
+            //info: ritorna le info del gestore con id == {id}
+
+            return "value";
+        }
+
+        // POST: api/Gestore
+        [HttpPost]
+        public void Post(Gestore gestore)
+        {
+        }
+
+        //// PUT: api/Gestore/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Gestore/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PointrestServerSide.Controllers
+{
+    public class UserController : ApiController
+    {
+        //// GET: api/User
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/User/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/User
+        [HttpGet]
+        public void Post(User user)
+        {
+            //return : “201” succesfull, “403” denied, “500” internal server error
+        }
+
+        //POST: api/User/5
+        [HttpPost]
+        public void Post( int id, [FromBody]string idPuntoInteresse )
+        {
+
+        }
+
+        //// PUT: api/User/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/User/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
