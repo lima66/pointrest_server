0fb2ffcc9d3f4cbea220555ac312d9e5589ebd18
StefanoVuerich <vuric@hotmail.it>
2015-06-04 17:00:24 +0200

    Addeded DTO

---
+++ DTO/PuntoInteresse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace PointrestServerSide.DTO
+{
+    public class PuntoInteresse
+    {
+        public int ID { get; set; }
+        public int IDGestore { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Sottocategoria { get; set; }
+        public string Descrizione { get; set; }
+        public double Latitudine { get; set; }
+        public double Longitudine { get; set; }
+        public string Tipo { get; set; }
+        public List<string> Images { get; set; }
+
+        public PuntoInteresse() { }
+
+        public PuntoInteresse(string ID, string IDGestore, string nome, string categoria, string sottocategoria, 
+            string descrizione, double latitudine, double longitudine, string tipo, List<byte[]> images)
+        {
+            this.ID = ID;
+            this.IDGestore = IDGestore;
+            this.Nome = nome;
+            this.Categoria = categoria;
+
+            if (sottocategoria != null)
+            {
+                this.Sottocategoria = sottocategoria;
+            }
+            
+            this.Descrizione = descrizione;
+            this.Latitudine = latitudine;
+            this.Longitudine = longitudine;
+            this.Tipo = tipo;
+
+            if (images != null)
+            {
+                this.Images = images;
+            }
+        }
+        
+
+    }
+}
---
+++ DTO/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PointrestServerSide.DTO
+{
+    public class User
+    {
+        public User(string ID, string userName, string name, string surname, List<PuntoInteresse> cPreferiti)
+        {
+            this.ID = ID;
+            this.userName = userName;
+            this.name = name;
+            this.surName = surName;
+            
+            if(cPreferiti != null) {
+                preferiti = cPreferiti;
+            }
+        }
+
+        public string ID { get; set; }
+        public string userName { get; set; }
+        public string name { get; set; }
+        public string surName { get; set; }
+        public List<PuntoInteresse> preferiti { get; set; }
+
+    }
+}
